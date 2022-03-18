5e4f80d32d5568110bd0650411d387f71f957173
aungkolatt25 <aungkolatt2504@gmail.com>
2022-03-18 12:19:05 +0630

    Learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+/**
+ * A decorator that appears immediately before a class definition, which declares the class to be of the given type, and provides metadata suitable to the type.
+ * @Component(),@Directive(),@Pipe(),@Injectable(),@NgModule()
+ */
+
+/**
+ * What are decorators?
+ * Decorators are a design pattern that is used to separate modification or decoration of a class without modifying the original source code.
+ * In AngularJS, decorators are functions that allow a service, directive or filter to be modified prior to its usage.
+ */
+
+/**
+ * The @Component decorator identifies the class immediately below it as a component class, and specifies its metadata.
+ * A Class's not a component until you mark it as one with the @Component decorator.
+ */
+/**@Component() class decorator */
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  /**
+   * A decorator statement immediately before a field in a class definition that declares the type of that field.
+   * Some examples are @Input and @Output
+   */
+
+  /**
+   * Property decorator that configures a view query.
+   * The change detector looks for the first element or the directive matching the selector in the view DOM.
+   *  If the view DOM changes, and a new child matches the selector, the property is updated.
+   */
   @ViewChild('ref') input: ElementRef;
 
   name = 'Angular ' + VERSION.major;
@@ -20,5 +46,9 @@ export class AppComponent {
 
   clickHello($event) {
     alert($event);
+  }
+
+  onSubmit(customform) {
+    console.log(customform.value);
   }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,19 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-/**
- * A decorator that appears immediately before a class definition, which declares the class to be of the given type, and provides metadata suitable to the type.
- * @Component(),@Directive(),@Pipe(),@Injectable(),@NgModule()
- */
 
-/**
- * The @Component decorator identifies the class immediately below it as a component class, and specifies its metadata.
- * A Class's not a component until you mark it as one with the @Component decorator.
- */
-/**@Component() class decorator */
 @Component({
   selector: 'hello',
   template: `<h1 (click)="clickMe()">Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`],
   /**templateUrl:"" */
+  /**styleUrl:"" */
+
   /**
    * An array of providers for services that the component requires.
    * In the example, this tells Angular how to provide the Service instance that the component's constructor uses to get or to do something.
@@ -22,17 +15,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class HelloComponent {
   /**
-   * A decorator statement immediately before a field in a class definition that declares the type of that field.
-   * Some examples are @Input and @Output
-   */
-
-  /**
    * Decorator that marks a class field as an input property and supplies configuration metadata.
    * The input property is bound to a DOM property in the template.
    * During change detection, Angular automatically updates the data property with the DOM property's value.
    * */
   /**@Input() class field decorator */
   @Input() name: string;
+  /**
+   * Output Decorator use to with EventEmitter.
+   * Mean is, event is handled by parent
+   */
   @Output() clickHello: EventEmitter<any> = new EventEmitter<any>();
 
   clickMe() {
